c0e9ec6667963f09a623ac7a551fd9b40680215b
Frozen25 <alexis_gavriel@hotmail.com>
2017-06-08 18:57:24 -0600

    admin Service

---
+++ src/java/Services/Administradores.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Estructuras.AdminBST;
+import Estructuras.Admin_Dist;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Alexis
+ */
+@WebService(serviceName = "Administradores")
+public class Administradores {
+
+    /**
+     * This is a sample web service operation
+     */
+   @WebMethod(operationName = "addAdmin")
+    public String add( String nombre, int codigo) {
+        try{
+            AdminBST newC = AdminBST.load_Adm();
+            Admin_Dist id = new Admin_Dist(nombre, codigo);
+            newC.insert(id);
+            return "Success";
+        }catch (Exception e) {
+            e.printStackTrace();
+            return "Failed addAdmin";
+        }
+    }
+    
+    
+    
+    
+}
