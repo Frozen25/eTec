6e6fa559cbffce7255771a6bc8653bfd747fa0ba
raxzers <gacova95@gmail.com>
2017-06-15 10:33:43 -0600

    agregado la busqueda por codigo

--- src/java/Services/ProduSearch.java
+++ src/java/Services/ProduSearch.java
@@ -20,13 +20,30 @@ public class ProduSearch {
     /**
      * This is a sample web service operation
      */
-    @WebMethod(operationName = "hello")
+    @WebMethod(operationName = "coincidendciasNombre")
     public String[] busquedas_coincidendcias( String txt,String AlgOrden) {
         try{
         
         ItemsDoubleLinkedList listaProducto =  ItemsDoubleLinkedList.loadItems();
         ItemsDoubleLinkedList.ListaGeneral = listaProducto;
         String lis =ItemsDoubleLinkedList.busquedaNombres(txt);
+        String[] items =ItemsDoubleLinkedList.listaray(lis);
+        ItemsDoubleLinkedList.SortA(AlgOrden, items);
+        //listaProducto.saveItem();
+        return items;
+         }catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+     @WebMethod(operationName = "coincidendciasCodigo")
+    public String[] busquedas_coincidendciasCod( int id,String AlgOrden) {
+        try{
+        
+        ItemsDoubleLinkedList listaProducto =  ItemsDoubleLinkedList.loadItems();
+        ItemsDoubleLinkedList.ListaGeneral = listaProducto;
+        String lis =ItemsDoubleLinkedList.busquedaCodigo(id);
         String[] items =ItemsDoubleLinkedList.listaray(lis);
         ItemsDoubleLinkedList.SortA(AlgOrden, items);
         //listaProducto.saveItem();
